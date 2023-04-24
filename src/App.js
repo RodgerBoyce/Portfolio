225b240d1ee30f4d619a80d9a87af1434c27ef46
Rodger Boyce <rodgerdboyce@gmail.com>
2023-04-24 10:11:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="app">
+      <b></b>
+      <h1>RODGER BOYCE</h1>
+      <b></b>
+      <h2>Website currently under development...</h2>
+    </div>
+  );
+}
+
+export default App;
